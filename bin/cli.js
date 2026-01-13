1935c1f5ae0edf2e8887a123356dbfab9d71f583
jh.jeong <jh.jeong@theinfoin.com>
2026-01-13 17:03:31 +0900

    fix: 폴더 경로 위치 수정

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { execSync } from "child_process";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import path from "path";
 import { fileURLToPath } from "url";
+import { spawn } from "child_process";
+import fs from "fs-extra";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,23 +16,26 @@ const argv = yargs(hideBin(process.argv))
   .option("watch", { alias: "w", type: "boolean", default: false })
   .help().argv;
 
-// 패키지 내부의 프로젝트 루트 (db-json-cli 자체)
 const packageRoot = path.join(__dirname, "..");
+const standalonePath = path.join(packageRoot, ".next", "standalone");
+
+// static 파일 복사
+const staticSrc = path.join(packageRoot, ".next", "static");
+const staticDest = path.join(standalonePath, ".next", "static");
+
+if (fs.existsSync(staticDest)) {
+  fs.removeSync(staticDest);
+}
+fs.copySync(staticSrc, staticDest);
 
 process.env.DB_PATH = path.resolve(argv.db);
 process.env.PORT = argv.port.toString();
+process.env.HOSTNAME = "0.0.0.0";
 
 console.log(`✅ db-json-cli running on http://localhost:${argv.port}\n`);
 
-try {
-  // packageRoot에서 Next.js 실행
-  execSync(`npx next start -p ${argv.port}`, {
-    stdio: "inherit",
-    cwd: packageRoot,
-    env: process.env,
-    shell: true,
-  });
-} catch (err) {
-  console.error("Error:", err.message);
-  process.exit(1);
-}
+spawn("node", [path.join(standalonePath, "server.js")], {
+  cwd: standalonePath,
+  stdio: "inherit",
+  env: process.env,
+});
