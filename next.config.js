3f9a6b284975dcb8892d1fa30292c06e338e8ce0
jh.jeong <jh.jeong@theinfoin.com>
2026-01-27 14:54:03 +0900

    test: 메인페이지 디버깅

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  reactStrictMode: true,
+  compress: false,
+  compiler: {
+    removeConsole: false,
+  },
+  // ✅ 환경변수를 명시적으로 노출
+  env: {
+    DB_PATH: process.env.DB_PATH,
+    PORT: process.env.PORT,
+  },
+  experimental: {
+    // ✅ 서버 액션에서 환경변수 사용 가능하게
+    serverActions: {
+      allowedOrigins: ["localhost:*"],
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
