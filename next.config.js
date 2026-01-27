a6a96fad2625b9296a85bc8a61a79a8903388810
jh.jeong <jh.jeong@theinfoin.com>
2026-01-27 15:00:39 +0900

    test: 메인페이지 디버깅

--- next.config.js
+++ next.config.js
@@ -5,17 +5,15 @@ const nextConfig = {
   compiler: {
     removeConsole: false,
   },
-  // ✅ 환경변수를 명시적으로 노출
   env: {
     DB_PATH: process.env.DB_PATH,
     PORT: process.env.PORT,
   },
   experimental: {
-    // ✅ 서버 액션에서 환경변수 사용 가능하게
     serverActions: {
       allowedOrigins: ["localhost:*"],
     },
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig; 
